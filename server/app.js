1cddd6928b3cdc7592601ef0ceea94064256281d
Rahul Singh <singh.rahul.mailbox@gmail.com>
2017-04-02 13:53:39 +0530

    CMT4: Client Server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+app.use(cors());
+
+var router = express.Router();
+
+router.get("/sum",function(req,res){
+	var a = req.query.a;
+	var b = req.query.b;
+
+	var sum = parseInt(a) + parseInt(b);
+	res.status(200).json(sum);
+
+});
+
+app.use("/",router);
+
+app.listen("4467",function(){
+	console.log("Express Node server started..");
+});
